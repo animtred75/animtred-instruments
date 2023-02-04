b15dc48608bbc66049af65e12758d6f652f50f60
animtred75 <99289853+animtred75@users.noreply.github.com>
2023-02-04 13:58:43 -0500

    fixed track

--- midi.js
+++ midi.js
@@ -1,5 +1,5 @@
 /*
-Midi Sound Engine v2.2.6
+Midi Sound Engine v2.2.8
 
 my custom sound engine
 
@@ -465,11 +465,6 @@ var MidSE = (function(){
 	const ASSET_URL = 'https://assets.scratch.mit.edu/internalapi/asset/$md5ext/get/';
 	function loadSoundbankFile(src) {
 		return fetch(src).then(function(r) { return r.arrayBuffer() });
-	}
-	function settled(promise) {
-		return new Promise(function(resolve, _reject) {
-			return promise.then(function() {resolve()}).catch(function() {resolve()});
-		});
 	}
 	const ADPCM_STEPS = [7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66, 73, 80, 88, 97, 107, 118, 130, 143, 157, 173, 190, 209, 230, 253, 279, 307, 337, 371, 408, 449, 494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878, 2066, 2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358, 5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767];
 	const ADPCM_INDEX = [-1, -1, -1, -1, 2, 4, 6, 8, -1, -1, -1, -1, 2, 4, 6, 8];
@@ -1020,8 +1015,8 @@ var MidSE = (function(){
 	MidiTrack.prototype.getNote = function(time, mute) {
 		var GS = [];
 		if (this.decodeNoteQueue.length > 0) {
-			if (this.decodeNoteQueue[this.noteTracker] && ((time * 1000000) > this.decodeNoteQueue[this.noteTracker][3])) {
-				while (!(!this.decodeNoteQueue[this.noteTracker] || (this.decodeNoteQueue[this.noteTracker] && ((time * 1000000) < this.decodeNoteQueue[this.noteTracker][3])))) {
+			if (this.decodeNoteQueue[this.noteTracker] && (Math.floor(time * 1000000) > this.decodeNoteQueue[this.noteTracker][3])) {
+				while (!(!this.decodeNoteQueue[this.noteTracker] || (this.decodeNoteQueue[this.noteTracker] && (Math.floor(time * 1000000) < this.decodeNoteQueue[this.noteTracker][3])))) {
 					if (!mute) {
 						GS.push(this.noteTracker);
 					}
@@ -1029,8 +1024,8 @@ var MidSE = (function(){
 				}
 			}
 			this.noteTracker -= 1;
-			if (this.decodeNoteQueue[this.noteTracker] && ((time * 1000000) < this.decodeNoteQueue[this.noteTracker][3])) {
-				while (!(!this.decodeNoteQueue[this.noteTracker] || (this.decodeNoteQueue[this.noteTracker] && ((time * 1000000) > this.decodeNoteQueue[this.noteTracker][3])))) {
+			if (this.decodeNoteQueue[this.noteTracker] && (Math.floor(time * 1000000) <= this.decodeNoteQueue[this.noteTracker][3])) {
+				while (!(!this.decodeNoteQueue[this.noteTracker] || (this.decodeNoteQueue[this.noteTracker] && (Math.floor(time * 1000000) > this.decodeNoteQueue[this.noteTracker][3])))) {
 					this.noteTracker -= 1;
 				}
 			}
@@ -1104,7 +1099,7 @@ var MidSE = (function(){
 		this.muteMusicr = true;
 		this.MidiTimer = s;
 		this.setStartTime(s);
-		if (this.MidiTimer < 0) {
+		if (this.MidiTimer <= 0) {
 			this.MidiTimer = 0;
 			this.resetNoteTracker();
 			this.setStartTime(0);
@@ -1276,7 +1271,7 @@ var MidSE = (function(){
 			if (this.onended) this.onended();
 			this.isPaused = true;
 		}
-		if (!this.isPaused) {
+		if (!this.isPaused && !this.muteMusicr) {
 			this.MidiTimer = (Date.now() - this.startTime) / 1000;
 		}
 		function GS(f, s) {
